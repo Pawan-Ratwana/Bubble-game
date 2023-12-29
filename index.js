4136c01e7c6287cc4a1c5a3c710607c6bfb97f05
Pawan Kumar <ratwanapawan@gmail.com>
2023-12-29 14:59:05 +0530

    add makebubble, runTier,gethit,inc score fncality

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("Jai Shree Ram")
+
+// function which make the bubble 
+function makeBubble() {
+    let cluster = ""
+    for (let i = 1; i <= 126; i++) {
+        const rn = Math.floor(Math.random() * 10);
+        cluster += `<div class="bubble">${rn}</div>`
+    }
+
+    document.querySelector("#pbtm").innerHTML = cluster
+}
+
+// to set run the timer
+let timer = 60;
+function runTimer() {
+    let innerTimer = setInterval(function () {
+        if (timer > 0) {
+            timer--;
+            document.querySelector("#timerVal").textContent = timer;
+        } else {
+            clearInterval(innerTimer);
+        }
+    }, 1000);
+}
+
+// used when someone hit the bubble then change the valve of hit button
+function getNewHit() {
+    let rn = Math.floor(Math.random() * 10);
+    document.querySelector("#hitVal").textContent = rn
+}
+let score = 0;
+function increaseScore() {
+    score += 10;
+    document.querySelector("#scoreVal").textContent = score;
+}
+
+makeBubble();
+runTimer()
+getNewHit();
+increaseScore()
